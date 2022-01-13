53c5b109d5f67b7f38c847bfa5781d84ff464b8b
Diego Parra <diegoparraferreira@gmail.com>
2022-01-13 08:35:47 -0400

    bump version

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
+		return c.SendString("Hello,👋! This is v2")
 	})
 
 	app.Listen(":3000")
